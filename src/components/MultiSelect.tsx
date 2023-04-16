74a2e55e0f1720213a3828347153518391f48d94
Ekaterina Melnichuk <ekaterina.melnichuk@applifting.cz>
2023-04-17 00:33:33 +0200

    wip

--- src/components/MultiSelect.tsx
+++ src/components/MultiSelect.tsx
@@ -15,6 +15,7 @@ export const MultiSelect = ({ data, label, placeholder }: Props) => {
   const [availableItems, setAvailableItems] = useState(data);
   const [selectedItems, setSelectedItems] = useState<SelectItem[]>([]);
   const [isOpen, setIsOpen] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
   const areItemsSelected = selectedItems.length > 0;
   const areItemsRemaining = availableItems.length > 0;
@@ -24,6 +25,9 @@ export const MultiSelect = ({ data, label, placeholder }: Props) => {
   const handleItemClick = useCallback(
     (e: React.MouseEvent, selectedValue: string, action: ItemAction) => {
       e.stopPropagation();
+      const itemInOriginalData = data.find(
+        (item) => item.value === selectedValue
+      );
       const arrayToRemoveItem =
         action === ItemAction.Add ? availableItems : selectedItems;
 
@@ -35,11 +39,12 @@ export const MultiSelect = ({ data, label, placeholder }: Props) => {
         const chosenItemToRemove = arrayToRemoveItem[chosenItemIndex];
         updatedItems.splice(chosenItemIndex, 1);
 
-        setAvailableItems((prevItems) =>
-          action === ItemAction.Add
-            ? updatedItems
-            : [...prevItems, chosenItemToRemove]
-        );
+        itemInOriginalData &&
+          setAvailableItems((prevItems) =>
+            action === ItemAction.Add
+              ? updatedItems
+              : [...prevItems, chosenItemToRemove]
+          );
         setSelectedItems((prevItems) =>
           action === ItemAction.Add
             ? [...prevItems, chosenItemToRemove]
@@ -47,8 +52,29 @@ export const MultiSelect = ({ data, label, placeholder }: Props) => {
         );
       }
     },
-    [availableItems, selectedItems]
+    [availableItems, selectedItems, data]
   );
+
+  const addOwnItem = (
+    e: React.KeyboardEvent<HTMLInputElement>,
+    inputValue: string
+  ) => {
+    const trimmedValue = inputValue.trim();
+    if (e.code === "Enter" && trimmedValue !== "") {
+      if (trimmedValue.length > 10)
+        return alert(`You are only allowed to add maximum 10 characters!`);
+      const duplicateItem = data.find(
+        (item) => item.label.toLowerCase() === trimmedValue.toLowerCase()
+      );
+      if (duplicateItem)
+        return alert(`You already have ${trimmedValue} in the option list!`);
+      setSelectedItems((prevItems) => [
+        ...prevItems,
+        { value: trimmedValue.toLowerCase(), label: trimmedValue },
+      ]);
+      setInputValue("");
+    }
+  };
 
   useOutsideClick(selectRef, () => setIsOpen(false));
 
@@ -57,13 +83,13 @@ export const MultiSelect = ({ data, label, placeholder }: Props) => {
       <p className="font-bold mb-6">{label}</p>
       <div
         className={clsx(
-          "flex items-center justify-between min-h-52 px-12 py-8 mb-10 rounded border-2 transition-colors cursor-pointer",
+          "flex items-center justify-between min-h-52 px-12 py-8 mb-10 rounded border-2 transition-colors",
           isOpen ? "border-blue" : "border-gray"
         )}
         onClick={() => setIsOpen(!isOpen)}
       >
-        <ul className="flex flex-wrap gap-8">
-          {areItemsSelected ? (
+        <ul className="flex flex-wrap gap-8 mr-6 w-full">
+          {areItemsSelected &&
             selectedItems.map((selectedItem) => {
               const { value, label } = selectedItem;
               return (
@@ -81,11 +107,17 @@ export const MultiSelect = ({ data, label, placeholder }: Props) => {
                   />
                 </li>
               );
-            })
-          ) : (
-            <li className="text-gray">{placeholder}</li>
-          )}
+            })}
+          <input
+            type="text"
+            className="border-none outline-none bg-transparent grow"
+            placeholder={!areItemsSelected ? placeholder : ""}
+            onKeyDown={(e) => addOwnItem(e, inputValue)}
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+          />
         </ul>
+
         <SelectIcon className="h-14 stroke-gray" />
       </div>
       {areItemsRemaining && (
